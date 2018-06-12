8409597958f4c9b8cbf76a08cf3d49bad99c3aef
Karurinka <michellebroens96@gmail.com>
2018-06-12 16:15:03 +0200

    Properties added

--- KeyGenerator/src/main/java/logic/RSA.java
+++ KeyGenerator/src/main/java/logic/RSA.java
@@ -2,6 +2,7 @@ package logic;
 
 import java.io.*;
 import java.security.*;
+import java.util.Properties;
 
 public class RSA
 {
@@ -9,22 +10,32 @@ public class RSA
 
     public static void main(String[] args)
     {
+        Properties properties = new Properties();
+        try (InputStream in = new FileInputStream("keys.properties"))
+        {
+            properties.load(in);
+        } catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
         try
         {
-            if (args[0].equals("-genkey"))
-            {
-                KeyPairGenerator pairgen = KeyPairGenerator.getInstance("RSA");
-                SecureRandom random = new SecureRandom();
-                pairgen.initialize(KEYSIZE, random);
-                KeyPair keyPair = pairgen.generateKeyPair();
+            KeyPairGenerator pairgen = KeyPairGenerator.getInstance("RSA");
+            SecureRandom random = new SecureRandom();
+            pairgen.initialize(KEYSIZE, random);
+            KeyPair keyPair = pairgen.generateKeyPair();
 
-                ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(args[1]));
-                out.writeObject(keyPair.getPublic());
-                out.close();
-                out = new ObjectOutputStream(new FileOutputStream(args[2]));
-                out.writeObject(keyPair.getPrivate());
-                out.close();
-            }
+            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(properties.getProperty("public")));
+            out.writeObject(keyPair.getPublic());
+            out.close();
+            out = new ObjectOutputStream(new FileOutputStream(properties.getProperty("private")));
+            out.writeObject(keyPair.getPrivate());
+            out.close();
+
         } catch (Exception e)
         {
             e.printStackTrace();
